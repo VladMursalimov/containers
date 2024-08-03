9e7a4b4df8fdabe6c1151a0d61bd3c3786b16fa0
mursa <mursalimov.vlad@yandex.ru>
2024-08-03 18:56:10 +0500

    123

---
+++ cmake-build-debug/CMakeFiles/s21_containers.dir/DependInfo.cmake
@@ -0,0 +1,24 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "D:/Projects CPP/containers/s21_containers/list.cpp" "CMakeFiles/s21_containers.dir/s21_containers/list.obj" "gcc" "CMakeFiles/s21_containers.dir/s21_containers/list.obj.d"
+  "D:/Projects CPP/containers/s21_containers/main.cpp" "CMakeFiles/s21_containers.dir/s21_containers/main.obj" "gcc" "CMakeFiles/s21_containers.dir/s21_containers/main.obj.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/s21_containers.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/s21_containers.dir/s21_containers/list.obj"
+  "CMakeFiles/s21_containers.dir/s21_containers/list.obj.d"
+  "CMakeFiles/s21_containers.dir/s21_containers/main.obj"
+  "CMakeFiles/s21_containers.dir/s21_containers/main.obj.d"
+  "libs21_containers.dll.a"
+  "s21_containers.exe"
+  "s21_containers.exe.manifest"
+  "s21_containers.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/s21_containers.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
